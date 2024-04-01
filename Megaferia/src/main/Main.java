a6a8fc0f3e01d35c4a8f7fc6bc917453a13b0f3f
Juan Diego Rojas De La Hoz <rojasdelahoz@uninorte.edu.co>
2024-04-01 13:49:31 -0500

    aaa

--- Megaferia/src/main/Main.java
+++ Megaferia/src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Test para ");
         
         System.out.println("trabajando en rama 1");
         
